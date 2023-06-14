659098ba31a5ee699cfa15c5120be676dfc35c50
zhuganglie <pyrrhonianpig@gmail.com>
2023-06-15 00:43:21 +0800

    update

--- next.config.js
+++ next.config.js
@@ -2,7 +2,11 @@
 const { withContentlayer } = require('next-contentlayer')
 
 module.exports = withContentlayer({
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
   reactStrictMode: true,
-  experimental: { appDir: true },
   
 })
