6ee4a28346e8d5c7f6c9eb76055364f9ab3beae0
zhuganglie <pyrrhonianpig@gmail.com>
2023-12-06 00:12:38 +0800

    update

--- app/lessons/page.tsx
+++ app/lessons/page.tsx
@@ -10,14 +10,14 @@ export default function Lessons(){
            <>
           
             <h2>Lessons</h2>
-            <button onClick={() => signOut()} className="border rounded border-slate-900 min-w-max p-1.5">Sign out</button>
+            <button onClick={() => signOut()} className="border rounded bg-slate-900 text-white min-w-max p-2.5">Sign out</button>
            </>
         )
     } else {
         return(
             <>
             <p className="font-bold">You are not allowed to view this page!</p>
-            <button onClick={() => signIn()} className="border rounded border-slate-900 min-w-max p-1.5">Sign in</button>
+            <button onClick={() => signIn()} className="border rounded bg-slate-900 text-white min-w-max p-2.5">Sign in</button>
             </>
         )
     }
