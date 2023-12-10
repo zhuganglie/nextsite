b9f2af25dc2f7672744b51ec69ff9fc00e329b6b
zhuganglie <pyrrhonianpig@gmail.com>
2023-12-10 14:58:23 +0800

    update

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,42 +1,6 @@
-import NextAuth, { type NextAuthOptions } from "next-auth";
-import CredentialsProvider from "next-auth/providers/credentials";
-import prisma from "@/utils/prisma";
-import { compare } from "bcrypt";
+import NextAuth from "next-auth";
+import { authOptions } from "../options/route"
 
-export const authOptions = {
-  providers: [
-    CredentialsProvider({
-      credentials: {
-        email: { label: "Email", type: "email" },
-        password: { label: "Password", type: "password" }
-      },
-      async authorize(credentials) {
-        const { email, password } = credentials ?? {}
-        if (!email || !password) {
-          throw new Error("Missing username or password");
-        }
-        const user = await prisma.user.findUnique({
-          where: {
-            email,
-          },
-        });
-        // if user doesn't exist or password doesn't match
-       /* if (!user || !compare(password, user.password)) {
-          throw new Error("Invalid username or password");
-        }
-        return user;*/
-        if (!user) {
-          throw new Error("Invalid username or password");
-        }
-        const passwordMatch = await compare(password, user.password);
-        if (!passwordMatch) {
-          throw new Error("Invalid username or password");
-        }
-        return user;
-      },
-    }),
-  ],
-};
 
 const handler = NextAuth(authOptions);
 
