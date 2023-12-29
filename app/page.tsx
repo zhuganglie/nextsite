cfee44a1777096ef421024ef8328d05c1eb96cd9
zhuganglie <pyrrhonianpig@gmail.com>
2023-12-29 16:31:46 +0800

    update

--- app/page.tsx
+++ app/page.tsx
@@ -39,7 +39,7 @@ const Home: React.FC = () => {
   };
 
   return (
-    <article className="">
+    <article className="p-2">
       <h2>Welcome</h2>
       <div className="flex flex-col justify-center items-center  ">
         {flashcards.length > 0 && (
