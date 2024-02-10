6e09a2851902bc0886ec089876fd10a9a53d5ee6
zhuganglie <pyrrhonianpig@gmail.com>
2024-02-10 15:43:36 +0800

    add google ai

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,15 +1,34 @@
+"use client"
 import React from 'react';
-import { Metadata } from "next";
+import { useChat } from 'ai/react';
 
-export const metadata: Metadata = {
-  title: "About",
-  description: "about page"
-};
 
 const About = () => {
+  const { messages, input, handleInputChange, handleSubmit} = useChat();
+  const refreshPage = () => {
+    window.location.reload(); }
+
   return (
       <article>
         <h2>About</h2>
+        <div className="grid gap-4">
+      {messages.map(m => (
+        <div key={m.id}>
+          {m.role === 'user' ? 'User: ' : 'AI: '}
+          {m.content }
+        </div>
+      ))}
+ 
+      <form onSubmit={handleSubmit}>
+        <input
+          value={input}
+          placeholder="Say something..."
+          onChange={handleInputChange}
+          className="p-4 min-w-full"
+        />    
+      </form>
+      <button onClick={refreshPage}>Reset</button>
+    </div>
       </article>
   );
 };
