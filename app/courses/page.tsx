3c6b0a309854c519290636d590c1823dec99cf68
zhuganglie <pyrrhonianpig@gmail.com>
2024-02-12 01:14:13 +0800

    update

--- app/courses/page.tsx
+++ app/courses/page.tsx
@@ -6,7 +6,7 @@ const Courses = async () => {
   return (
     <article className="flex min-h-screen flex-col items-center">
       <h2>Courses</h2>
-      
+      <p>This page is under construction ... </p>
     </article>
   )
 }
