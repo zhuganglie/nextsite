9b124e7784af00228b2eab7106799e8a5e2acecf
zhuganglie <pyrrhonianpig@gmail.com>
2024-06-08 22:37:47 +0800

    update

--- app/api/create-user/route.js
+++ app/api/create-user/route.js
@@ -3,7 +3,7 @@ import { NextResponse } from "next/server";
 
 export async function GET() {
   try {
-    await sql`CREATE TABLE User (userId varchar(255), data jsonb);`;
+    await sql`CREATE TABLE UserData (userId varchar(255), data jsonb);`;
     return NextResponse.json({ success: true }, { status: 200 });
   } catch (error) {
     return NextResponse.json({ error: error.message }, { status: 500 });
